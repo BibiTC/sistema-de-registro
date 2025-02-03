89519931898a93c84f78b9af670658e9440dc4e4
BibiTC <bibiana.trujillo.bt@gmail.com>
2025-02-03 17:32:33 -0500

    Actualizar el app.js adicion console log cambio desde Visual de Git Hub directamente

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log('Ejecutando el Sistema')
 console.log('Validación Control de Versiones')
 console.log('Adición Cambio desde GIT HUB directamente')
+console.log('Adición Cambio desde VISUAL DE GIT HUB directamente')
